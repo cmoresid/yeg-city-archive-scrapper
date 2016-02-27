46d8255a9542acba6af9d44f1a1e7180e8d176c4
Connor Moreside <cmoresid@ualberta.ca>
2016-02-27 10:19:52 -0700

    Updated requirements. Updated download script to fix SSLError issue in Mac OS X El Capitan.

--- download_images.py
+++ download_images.py
@@ -2,20 +2,41 @@ import csv
 import os
 import os.path
 import requests
+import ssl
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.poolmanager import PoolManager
 
 CRAWLER_OUTPUT = 'data/yeg_city_archive.csv'
 OUTPUT_DIRECTORY = 'data/imgs'
+
+
+class SSLErrorAdaptor(HTTPAdapter):
+    """
+    Used to bypass an issue with SSL in Mac OS X El Capitan.
+    """
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = PoolManager(num_pools=connections,
+                                       maxsize=maxsize,
+                                       block=block,
+                                       ssl_version=ssl.PROTOCOL_TLSv1)
+
+
+req = requests.Session()
+# Bypass SSLError: bad handshake error
+req.mount('https://', SSLErrorAdaptor())
+
 
 def download_image(record):
     print 'Downloading %s . . .' % (record['image_number'])
 
     try:
-        img_request = requests.get(record['image_url'])
+        img_request = req.get(record['image_url'])
 
         with open(os.path.join(OUTPUT_DIRECTORY, record['image_number'] + '.jpeg'), 'wb') as img_file:
             img_file.write(img_request.content)
-    except Exception:
+    except Exception as ex:
         print '*** Unable to download file %s' % record['image_number']
+        print ex.message
 
 
 def main():
